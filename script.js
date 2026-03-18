7fe70824e7e4feb3bf0185b3b42ef8950203e21b
whatever12334 <eric.chalmers.poston@gmail.com>
2026-03-18 15:26:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+(() => {
+  const navToggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('.site-nav');
+
+  if (navToggle && nav) {
+    navToggle.addEventListener('click', () => {
+      const isOpen = nav.classList.toggle('open');
+      navToggle.setAttribute('aria-expanded', String(isOpen));
+    });
+
+    nav.querySelectorAll('a').forEach(link => {
+      link.addEventListener('click', () => {
+        nav.classList.remove('open');
+        navToggle.setAttribute('aria-expanded', 'false');
+      });
+    });
+  }
+})();
